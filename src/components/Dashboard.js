446ee40bfc219aa6ae70b1ed0cc6e09228f59b6f
Thisath Thiyunajith <thisaththiyunajith@gmail.com>
2021-05-29 13:03:51 +0530

    Cahnaged the layout of the dashbord

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,7 +5,8 @@ import {Link, useHistory} from'react-router-dom'
 
 export default function Dashboard() {
     const [error, setError] = useState("")
-    const {logout} = useAuth()
+    const {currentUser,logout} = useAuth()
+    //const {logout} = useAuth()
     const history = useHistory()
 
     async function handleLogout(){
@@ -23,9 +24,12 @@ export default function Dashboard() {
     return (
         <>
             <Card>
-                <h2 className="text-center mb-4">Dashboard</h2>
-                {error && <Alert variant="danger">{error}</Alert>}
-                <Link to="/Update-profile" className="btn btn-primary w-100 mt-3"> Update Profile </Link>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Dashboard</h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <strong>Email:</strong> {currentUser}
+                    <Link to="/Update-profile" className="btn btn-primary w-100 mt-3"> Update Profile </Link>
+                </Card.Body>
             </Card>
 
             <div className= "w-100 text-center mt-2">
