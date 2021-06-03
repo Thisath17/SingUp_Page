68f9689088445d71536e0a9c4141964471aab082
Thisath Thiyunajith <thisaththiyunajith@gmail.com>
2021-06-03 21:11:26 +0530

    Created a card boby

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,14 +4,20 @@ import {useAuth} from "../contexts/AuthContexts"
 import {Link, useHistory} from'react-router-dom'
 
 export default function Dashboard() {
+
+
+
     return (
         <>
             <Card>
-                
+                <Card.Body>
+                    <h2 className="text-center mb-4">Welcome to Dashboard</h2>
+                </Card.Body>
             </Card>
 
+            // Creating logout button 
             <div className= "w-100 text-center mt-2">
-                <Button >Log Out</Button>
+                <Button variant="link" onClick={handleLogout}>Log Out</Button>
             </div> 
         </>
     )
