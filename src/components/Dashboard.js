36bbde11e86be805c519f99078903fb63c67224d
Thisath Thiyunajith <thisaththiyunajith@gmail.com>
2021-06-03 21:07:51 +0530

    Started from the begening

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,36 +4,14 @@ import {useAuth} from "../contexts/AuthContexts"
 import {Link, useHistory} from'react-router-dom'
 
 export default function Dashboard() {
-    const [error, setError] = useState("")
-    const {currentUser,logout} = useAuth()
-    //const {logout} = useAuth()
-    const history = useHistory()
-
-    async function handleLogout(){
-        setError('')
-
-        try{
-            await logout()
-            history.push('/login') 
-        }catch{
-            setError('Failed to Log Out')
-        }
-
-    }
-
     return (
         <>
             <Card>
-                <Card.Body>
-                    <h2 className="text-center mb-4">Dashboard</h2>
-                    {error && <Alert variant="danger">{error}</Alert>}
-                    <strong>Email:</strong> {currentUser}
-                    <Link to="/Update-profile" className="btn btn-primary w-100 mt-3"> Update Profile </Link>
-                </Card.Body>
+                
             </Card>
 
             <div className= "w-100 text-center mt-2">
-            <Button variant="link" onClick={handleLogout}>Log Out</Button>   
+                <Button >Log Out</Button>
             </div> 
         </>
     )
